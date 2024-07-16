5876b9e668cf9079d1992124a66654617bf8061c
BigBeile <83521911+BigBeile@users.noreply.github.com>
2024-07-16 20:47:04 +0200

    update keymap test

---
+++ config/base_5x3T2.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <behaviors.dtsi>
+#include <dt-bindings/zmk/keys.h>
+#include <dt-bindings/zmk/bt.h>
+#include <dt-bindings/zmk/outputs.h>
+
+#define APT 0
+#define NUM 1
+#define SYM 2
+
+
+
+
+
